7110e898a4b0ffb3ed155b97842817a68b8e2693
Artem <gretskovart@gmail.com>
2019-10-29 00:15:35 +0400

    Refactoring Tournaments View with material-table

--- src/views/Tournaments/Tournaments.js
+++ src/views/Tournaments/Tournaments.js
@@ -3,21 +3,14 @@ import { observer } from "mobx-react";
 import {
   Button,
   Container,
-  IconButton,
-  Link as StyledLink,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
   CircularProgress,
   Typography
 } from "@material-ui/core";
-import { withStyles } from '@material-ui/core/styles';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import CreateIcon from '@material-ui/icons/Create';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { StyledTableRow, styles } from "./Tournaments.styles";
+import { withStyles } from "@material-ui/core/styles";
+import { grey } from "@material-ui/core/colors";
+import { AddCircleOutline, Create, Delete } from "@material-ui/icons";
+import MaterialTable from "material-table";
+import { styles } from "./Tournaments.styles";
 import { store } from "../../store/tournamentStore";
 import dateFormat from "dateformat";
 import { withRouter, Link } from "react-router-dom";
@@ -28,6 +21,45 @@ import TournamentAddForm from "./TournamentAddForm";
 class Tournaments extends Component {
   constructor(props) {
     super(props);
+    this.tableConfig = {
+      addButton: {
+        icon: () => {
+          return (
+          <Button
+            color="primary"
+            startIcon={
+              <AddCircleOutline
+                color="primary"
+              />
+            }
+          >
+            Add new tournament
+          </Button>
+        )},
+        tooltip: "Add Tournament",
+        isFreeAction: true,
+        onClick: this.handleOpen
+      },
+      columns: [
+        { title: "№", field: "number" },
+        { title: "Title", field: "title" },
+        { title: "Start Date", field: "startDate", type: "date" },
+        { title: "Finish Date", field: "endDate", type: "date" },
+        { title: "Status", field: "status" },
+      ],
+      options: {
+        actionsColumnIndex: -1,
+        draggable: false,
+        paging: false,
+        rowStyle: {
+          backgroundColor: grey[200]
+        },
+        search: false,
+        showTitle: false,
+        sorting: false,
+        toolbarButtonAlignment: "left"
+      }
+    }
     this.state = {
       sortingProperty: "startDate",
       isLoading: true,
@@ -67,205 +99,69 @@ class Tournaments extends Component {
       return <CircularProgress className={classes.circularProgress} />;
     }
 
-    return store.tournaments.length ? (
+    return (
       <React.Fragment>
         <Container
           maxWidth="md"
           className={classes.container}
         >
-          <Button
-            color="primary"
-            startIcon={
-              <AddCircleOutlineIcon
-                color="primary"
-              />
-            }
-            onClick={this.handleOpen}
-          >
-            Add new tournament
-          </Button>
           <div style={{ overflowX: "auto" }}>
-            <Table className={classes.table} aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <TableCell
-                    className={classes.tableHeaderCell}
-                  >
-                    <Button
-                      color={
-                        this.state.sortingProperty === "number"
-                          ? "primary"
-                          : "default"
-                      }
-                      onClick={() => {
-                        this.setState({ sortingProperty: "number" });
-                      }}
-                    >
-                      №
-                    </Button>
-                  </TableCell>
-                  <TableCell
-                    className={classes.tableHeaderCell}
-                  >
-                    <Button
-                      className={classes.tableHeaderTitle}
-                      color={
-                        this.state.sortingProperty === "title"
-                          ? "primary"
-                          : "default"
-                      }
-                      onClick={() => {
-                        this.setState({ sortingProperty: "title" });
-                      }}
-                    >
-                      Title
-                    </Button>
-                  </TableCell>
-                  <TableCell
-                    className={classes.tableHeaderCell}
-                  >
-                    <Button
-                      className={classes.tableHeaderTitle}
-                      color={
-                        this.state.sortingProperty === "startDate"
-                          ? "primary"
-                          : "default"
-                      }
-                      onClick={() => {
-                        this.setState({ sortingProperty: "startDate" });
-                      }}
-                    >
-                      Start Date
-                    </Button>
-                  </TableCell>
-                  <TableCell
-                    className={classes.tableHeaderCell}
-                  >
-                    <Button
-                      className={classes.tableHeaderTitle}
-                      color={
-                        this.state.sortingProperty === "finishDate"
-                          ? "primary"
-                          : "default"
-                      }
-                      onClick={() => {
-                        this.setState({ sortingProperty: "finishDate" });
-                      }}
-                    >
-                      Finish Date
-                    </Button>
-                  </TableCell>
-                  <TableCell
-                    className={classes.tableHeaderCell}
-                  >
-                    <Button
-                      className={classes.tableHeaderTitle}
-                      color={
-                        this.state.sortingProperty === "status"
-                          ? "primary"
-                          : "default"
-                      }
-                      onClick={() => {
-                        this.setState({ sortingProperty: "status" });
-                      }}
-                    >
-                      Status
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {store.tournaments.map((tour, index) => (
-                  <StyledTableRow key={index}>
-                    <TableCell
-                      style={{
-                        textAlign: "center",
-                        fontWeight: this.state.sortingProperty === "number" && "bold"
-                      }}
-                    >
-                      {++index}
-                    </TableCell>
-                    <TableCell
-                      style={{
-                        fontWeight: this.state.sortingProperty === "title" && "bold"
-                      }}
-                    >
-                      <StyledLink
-                        color="textPrimary"
-                        underline="none"
-                        component={Link}
-                        to={`/tournaments/${tour.id}`}
-                      >
-                        {tour.title}
-                      </StyledLink>
-                    </TableCell>
-                    <TableCell
-                      style={{
-                        fontWeight: this.state.sortingProperty === "startDate" && "bold"
-                      }}
-                    >
-                      {dateFormat(tour.startDate, "dddd, mmmm dS, yyyy")}
-                    </TableCell>
-                    <TableCell
-                      style={{
-                        fontWeight: this.state.sortingProperty === "finishDate" && "bold"
-                      }}
-                    >
-                      {dateFormat(tour.endDate, "dddd, mmmm dS, yyyy")}
-                    </TableCell>
-                    <TableCell
-                      style={{
-                        fontWeight: this.state.sortingProperty === "status" && "bold"
-                      }}
-                    >
-                      {tour.status}
-                    </TableCell>
-                    <TableCell>
-                      <IconButton className={classes.button}>
-                        <CreateIcon fontSize="small" />
-                      </IconButton>
-                    </TableCell>
-                    <TableCell>
-                      <IconButton className={classes.button}>
-                        <DeleteIcon fontSize="small" />
-                      </IconButton>
-                    </TableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-          {this.state.open && (
-            <TournamentAddForm
-              handleClose={this.handleClose}
-              open={this.state.open}
-            />
-          )}
-        </Container>
-      </React.Fragment>
-    ) : (
-      <React.Fragment>
-        <Container
-          maxWidth="md"
-          className={classes.container}
-        >
-          <Button
-            color="primary"
-            startIcon={
-              <AddCircleOutlineIcon
-                color="primary"
+            {store.tournaments.length ? (
+              <MaterialTable
+                actions={[
+                  {
+                    ...this.tableConfig.addButton
+                  },
+                  {
+                    icon: Create,
+                    tooltip: "Edit Tournament",
+                    onClick: () => {}
+                  },
+                  {
+                    icon: Delete,
+                    tooltip: "Delete Tournament",
+                    onClick: () => {}
+                  }
+                ]}
+                columns={
+                  this.tableConfig.columns
+                }
+                data={
+                  store.tournaments.sort((a, b) => {
+                    return new Date(a.startDate) - new Date(b.startDate);
+                  }).map((tour, index) => (
+                    {
+                      number: ++index,
+                      title: tour.title,
+                      startDate: dateFormat(tour.startDate, "dddd, mmmm dS, yyyy"),
+                      endDate: dateFormat(tour.endDate, "dddd, mmmm dS, yyyy")
+                    }
+                  ))
+                }
+                options={
+                  this.tableConfig.options
+                }
               />
-            }
-            onClick={this.handleOpen}
-          >
-            Add new tournament
-          </Button>
-          <Typography
-            variant="subheading"
-            className={classes.message}
-          >
-            There are no tournaments yet
-          </Typography>
+            ) : (
+              <MaterialTable
+                actions={[
+                  this.tableConfig.addButton
+                ]}
+                columns={
+                  this.tableConfig.columns
+                }
+                localization={{
+                  body: {
+                    emptyDataSourceMessage: "There are no tournaments yet"
+                  }
+                }}
+                options={{
+                  ...this.tableConfig.options,
+                  header: false
+                }}
+              />
+            )}
+          </div>
           {this.state.open && (
             <TournamentAddForm
               handleClose={this.handleClose}
