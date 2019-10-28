0cf58c5fde4bf60bb4c63186f1d1758ce82dbbcc
blotteracid <klubniiikaaa@mail.ru>
2019-10-28 21:09:48 +0400

    Add material-table

--- src/views/Players/Players.js
+++ src/views/Players/Players.js
@@ -1,36 +1,18 @@
-import React, { Component } from "react"
+import React, { forwardRef, Component } from "react"
 import { store } from "../../store/userStore";
-import { 
-  Container, 
-  TextField,
-  InputAdornment,
-  Checkbox,
-  Grid,
-  Paper,
-  withStyles,
-  Avatar,
-  Box,
-  FormControlLabel,
-  Typography,
-  IconButton,
-  CircularProgress
-} from '@material-ui/core'
-import SearchIcon from '@material-ui/icons/Search';
-import EditIcon from '@material-ui/icons/Edit';
+import MaterialTable from "material-table"
+import { Container, withStyles, Avatar, CircularProgress } from "@material-ui/core"
+import SearchIcon from "@material-ui/icons/Search";
+import EditIcon from "@material-ui/icons/Edit";
+import ClearIcon from "@material-ui/icons/Clear";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 
-const styles = theme => ({
-  root: {
-    padding: theme.spacing(3, 2)
-  },
-  grid: {
+const styles = () => ({
+  container: {
     marginTop: "16px"
-  },
-  boxTitle: {
-    color: "#ccc",
-    fontSize: "14px"
   }
 });
-
 
 class Players extends Component {
   constructor(props) {
@@ -55,75 +37,72 @@ class Players extends Component {
 
   render() {
     const { classes } = this.props;
-    const { users } = store;
 
     if (this.state.isLoading) {
       return <CircularProgress style={{ margin: "15px auto" }} />;
     }
 
     return (
-      <Container>
-        <Grid className={classes.grid} container justify="space-between">
-          <TextField
-            variant="outlined"
-            placeholder="Search"
-            InputProps={{
-              startAdornment: <InputAdornment position="start">
-                <SearchIcon />
-                </InputAdornment>,
-            }}
-          />
-          <FormControlLabel
-            control={<Checkbox color="primary" />}
-            label="Show only active players"
-            labelPlacement="start"
-          />
-        </Grid>
-        <Grid className={classes.grid} container spacing={1} direction="column">
-          <Grid item xs={12}>
-            <Box display="flex" p={1} alignItems="flex-end">
-              <Box display="flex" width={100} justifyContent="center" color="grey">
-                <Typography className={classes.boxTitle}>
-                  Photo
-                </Typography>
-              </Box>
-              <Box display="flex" minWidth={300}>
-                <Typography className={classes.boxTitle}>
-                  Name
-                </Typography>
-              </Box>
-              <Box display="flex" minWidth={500}>
-                <Typography className={classes.boxTitle}>
-                  E-mail
-                </Typography>
-              </Box>
-            </Box>
-          </Grid>
-          {users && users.map(user => (
-            <Grid item xs={12} key={user.id}>
-              <Box display="flex" p={1} bgcolor="grey.300" alignItems="center">
-                <Box display="flex" width={100} justifyContent="center">
-                  <Avatar alt="Player avatar" src={user.photoUrl} className={classes.avatar} />
-                </Box>
-                <Box display="flex" minWidth={300}>
-                  <Typography>
-                    {user.name}
-                  </Typography>
-                </Box>
-                <Box display="flex" minWidth={500}>
-                  <Typography>
-                    {user.email}
-                  </Typography>
-                </Box>
-                <Box ml="auto">
-                <IconButton className={classes.button}>
-                  <EditIcon />
-                </IconButton>
-                </Box>
-              </Box>
-            </Grid>
-          ))}
-        </Grid>
+      <Container className={ classes.container }>
+        <MaterialTable 
+          title=""
+          pagination={{
+            labelRowsPerPage: "1"}
+          }
+          icons={{
+            Search: forwardRef((props, ref) => <SearchIcon {...props} ref={ref} />),
+            ResetSearch: forwardRef((props, ref) => <ClearIcon {...props} ref={ref} />),
+            NextPage: forwardRef((props, ref) => <ChevronRightIcon {...props} ref={ref} />),
+            PreviousPage: forwardRef((props, ref) => <ChevronLeftIcon {...props} ref={ref} />)
+          }}
+          columns={[
+            { 
+              title: "User", 
+              field: "photoUrl",
+              headerStyle: {
+                color: "#808080"
+              }, 
+              render: rowData => (
+                <Avatar alt="Player avatar" src={rowData.photoUrl} />
+              )
+            },
+            {
+              title: "Name", 
+              field: "name",
+              headerStyle: {
+                color: "#808080"
+              }, 
+            },
+            {
+              title: "E-mail", 
+              field: "email",
+              headerStyle: {
+                color: "#808080"
+              }, 
+            }
+          ]}
+          data={store.users}
+          options={
+            {
+              rowStyle: {
+                backgroundColor: "#eee"
+              }, 
+              searchFieldAlignment: "left",
+              actionsColumnIndex: -1,
+              pageSize: 20,
+              pageSizeOptions: [],
+              paginationType: "stepped",
+              sorting: false
+            }
+          }
+          actions={[
+            {
+              icon: () => <EditIcon />,
+              tooltip: "Edit User",
+              onClick: (event, rowData) => alert("Edit" + rowData.name)
+            }
+          ]}
+        />
       </Container>
     )
   }
