8ac8f51e2aa9537df969c998343c05d71b334b7f
Dmitry <amdv8@mail.ru>
2019-10-29 19:37:56 +0400

    Change "delete" button

--- src/views/Games/Game.js
+++ src/views/Games/Game.js
@@ -3,10 +3,9 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import ListItem from "@material-ui/core/ListItem";
-import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Badge from "@material-ui/core/Badge";
-import { Cancel } from "@material-ui/icons";
 import { withStyles } from "@material-ui/core/styles";
 import dateFormat from "dateformat";
 import UserAvatar from "../../components/UserAvatar";
@@ -159,9 +158,9 @@ class Game extends Component {
           </CardContent>
 
           <CardActions>
-            <IconButton onClick={this.openDeleteModal}>
-              <Cancel />
-            </IconButton>
+            <Button size="small" color="primary" onClick={this.openDeleteModal}>
+              Delete
+            </Button>
           </CardActions>
         </Card>
 
