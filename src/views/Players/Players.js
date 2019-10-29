555ba2eba8bdc81698438600a7be6b504d173e26
blotteracid <klubniiikaaa@mail.ru>
2019-10-29 18:29:48 +0400

    Delete styles, fix import

--- src/views/Players/Players.js
+++ src/views/Players/Players.js
@@ -1,23 +1,13 @@
 import React, { forwardRef, Component } from "react";
 import { store } from "../../store/userStore";
 import MaterialTable from "material-table";
-import {
-  Container,
-  withStyles,
-  Avatar,
-  CircularProgress
-} from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import SearchIcon from "@material-ui/icons/Search";
 import EditIcon from "@material-ui/icons/Edit";
 import ClearIcon from "@material-ui/icons/Clear";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-
-const styles = () => ({
-  container: {
-    marginTop: "16px"
-  }
-});
 
 class Players extends Component {
   constructor(props) {
@@ -41,83 +31,64 @@ class Players extends Component {
   }
 
   render() {
-    const { classes } = this.props;
-
     if (this.state.isLoading) {
       return <CircularProgress style={{ margin: "15px auto" }} />;
     }
 
     return (
-      <Container className={classes.container}>
-        <MaterialTable
-          title=""
-          pagination={{
-            labelRowsPerPage: "1"
-          }}
-          icons={{
-            Search: forwardRef((props, ref) => (
-              <SearchIcon {...props} ref={ref} />
-            )),
-            ResetSearch: forwardRef((props, ref) => (
-              <ClearIcon {...props} ref={ref} />
-            )),
-            NextPage: forwardRef((props, ref) => (
-              <ChevronRightIcon {...props} ref={ref} />
-            )),
-            PreviousPage: forwardRef((props, ref) => (
-              <ChevronLeftIcon {...props} ref={ref} />
-            ))
-          }}
-          columns={[
-            {
-              title: "User",
-              field: "photoUrl",
-              headerStyle: {
-                color: "#808080"
-              },
-              render: rowData => (
-                <Avatar alt="Player avatar" src={rowData.photoUrl} />
-              )
-            },
-            {
-              title: "Name",
-              field: "name",
-              defaultSort: "asc",
-              headerStyle: {
-                color: "#808080"
-              }
-            },
-            {
-              title: "E-mail",
-              field: "email",
-              headerStyle: {
-                color: "#808080"
-              }
-            }
-          ]}
-          data={store.users}
-          options={{
-            rowStyle: {
-              backgroundColor: "#eee"
-            },
-            searchFieldAlignment: "left",
-            actionsColumnIndex: -1,
-            pageSize: 20,
-            pageSizeOptions: [],
-            paginationType: "stepped",
-            sorting: false
-          }}
-          actions={[
-            {
-              icon: () => <EditIcon />,
-              tooltip: "Edit User",
-              onClick: (event, rowData) => alert("Edit" + rowData.name)
-            }
-          ]}
-        />
-      </Container>
+      <MaterialTable
+        title=""
+        icons={{
+          Search: forwardRef((props, ref) => (
+            <SearchIcon {...props} ref={ref} />
+          )),
+          ResetSearch: forwardRef((props, ref) => (
+            <ClearIcon {...props} ref={ref} />
+          )),
+          NextPage: forwardRef((props, ref) => (
+            <ChevronRightIcon {...props} ref={ref} />
+          )),
+          PreviousPage: forwardRef((props, ref) => (
+            <ChevronLeftIcon {...props} ref={ref} />
+          ))
+        }}
+        columns={[
+          {
+            title: "User",
+            field: "photoUrl",
+            render: rowData => (
+              <Avatar alt="Player avatar" src={rowData.photoUrl} />
+            )
+          },
+          {
+            title: "Name",
+            field: "name",
+            defaultSort: "asc"
+          },
+          {
+            title: "E-mail",
+            field: "email"
+          }
+        ]}
+        data={store.users}
+        options={{
+          searchFieldAlignment: "left",
+          actionsColumnIndex: -1,
+          pageSize: 20,
+          pageSizeOptions: [],
+          paginationType: "stepped",
+          sorting: false
+        }}
+        actions={[
+          {
+            icon: () => <EditIcon />,
+            tooltip: "Edit User",
+            onClick: (event, rowData) => alert("Edit" + rowData.name)
+          }
+        ]}
+      />
     );
   }
 }
 
-export default withStyles(styles)(Players);
+export default Players;
