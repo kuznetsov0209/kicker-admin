1b52cf62c8ebc26531d3e20673311f9c60ca6a5e
blotteracid <klubniiikaaa@mail.ru>
2019-10-29 17:00:49 +0400

    Fix route

--- src/entry/App.js
+++ src/entry/App.js
@@ -2,10 +2,9 @@ import React, { Component } from "react";
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import localeRu from "date-fns/locale/ru";
 import Games from "../views/Games";
-import Players from "../views/Players"
+import Players from "../views/Players";
 import Tournaments from "../views/Tournaments";
 import Tournament from "../views/Tournament";
-import Leaders from "../views/Leaders";
 import AppBar from "./AppBar";
 
 import "./App.css";
@@ -23,10 +22,9 @@ class App extends Component {
             <Switch>
               <Route exact path="/games" component={Games} />
               <Route exact path="/players" component={Players} />
-              <Route exact path="/leaders" component={Leaders} />
               <Route exact path="/tournaments" component={Tournaments} />
               <Route exact path="/tournaments/:id" component={Tournament} />
-              <Redirect to="/leaders" />
+              <Redirect to="/players" />
             </Switch>
           </div>
         </HashRouter>
