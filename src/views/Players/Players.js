c832cda9623e9c21108047cda0c98ed0ab554e95
DmitryDoskolovich <dmitriy.doskolovich@gmail.com>
2019-11-01 11:54:35 +0400

    add Player to Players list

--- src/views/Players/Players.js
+++ src/views/Players/Players.js
@@ -8,12 +8,15 @@ import EditIcon from "@material-ui/icons/Edit";
 import ClearIcon from "@material-ui/icons/Clear";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import Player from "./Player";
 
 class Players extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: true
+      isLoading: true,
+      playerIsOpen: false,
+      player: {}
     };
   }
 
@@ -30,63 +33,77 @@ class Players extends Component {
     this.loadUsersIfNeeded();
   }
 
+  closePlayerItem = () => {
+    this.setState({ playerIsOpen: false });
+  };
+
   render() {
     if (this.state.isLoading) {
       return <CircularProgress style={{ margin: "15px auto" }} />;
     }
 
     return (
-      <MaterialTable
-        title=""
-        icons={{
-          Search: forwardRef((props, ref) => (
-            <SearchIcon {...props} ref={ref} />
-          )),
-          ResetSearch: forwardRef((props, ref) => (
-            <ClearIcon {...props} ref={ref} />
-          )),
-          NextPage: forwardRef((props, ref) => (
-            <ChevronRightIcon {...props} ref={ref} />
-          )),
-          PreviousPage: forwardRef((props, ref) => (
-            <ChevronLeftIcon {...props} ref={ref} />
-          ))
-        }}
-        columns={[
-          {
-            title: "User",
-            field: "photoUrl",
-            render: rowData => (
-              <Avatar alt="Player avatar" src={rowData.photoUrl} />
-            )
-          },
-          {
-            title: "Name",
-            field: "name",
-            defaultSort: "asc"
-          },
-          {
-            title: "E-mail",
-            field: "email"
-          }
-        ]}
-        data={store.users}
-        options={{
-          searchFieldAlignment: "left",
-          actionsColumnIndex: -1,
-          pageSize: 20,
-          pageSizeOptions: [],
-          paginationType: "stepped",
-          sorting: false
-        }}
-        actions={[
-          {
-            icon: () => <EditIcon />,
-            tooltip: "Edit User",
-            onClick: (event, rowData) => alert("Edit" + rowData.name)
-          }
-        ]}
-      />
+      <div>
+        <MaterialTable
+          title=""
+          icons={{
+            Search: forwardRef((props, ref) => (
+              <SearchIcon {...props} ref={ref} />
+            )),
+            ResetSearch: forwardRef((props, ref) => (
+              <ClearIcon {...props} ref={ref} />
+            )),
+            NextPage: forwardRef((props, ref) => (
+              <ChevronRightIcon {...props} ref={ref} />
+            )),
+            PreviousPage: forwardRef((props, ref) => (
+              <ChevronLeftIcon {...props} ref={ref} />
+            ))
+          }}
+          columns={[
+            {
+              title: "User",
+              field: "photoUrl",
+              render: rowData => (
+                <Avatar alt="Player avatar" src={rowData.photoUrl} />
+              )
+            },
+            {
+              title: "Name",
+              field: "name",
+              defaultSort: "asc"
+            },
+            {
+              title: "E-mail",
+              field: "email"
+            }
+          ]}
+          data={store.users}
+          options={{
+            searchFieldAlignment: "left",
+            actionsColumnIndex: -1,
+            pageSize: 20,
+            pageSizeOptions: [],
+            paginationType: "stepped",
+            sorting: false
+          }}
+          actions={[
+            {
+              icon: () => <EditIcon />,
+              tooltip: "Edit User",
+              onClick: (rowData) => {
+                this.setState({ playerIsOpen: true, player: rowData });
+              }
+            }
+          ]}
+        />
+        {this.state.playerIsOpen ? (
+          <Player
+            player={this.state.player}
+            handleClosePlayer={this.closePlayerItem}
+          />
+        ) : null}
+      </div>
     );
   }
 }
