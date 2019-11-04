38ebc1fd001527a336ab804cad18c899daff83ad
iukhov <ivan.ukhov@csssr.com>
2019-11-04 19:40:17 +0400

    edit Table in Standings

--- src/views/Tournament/Standings.js
+++ src/views/Tournament/Standings.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import {
   Button,
+  IconButton,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableRow,
-  Typography
+  Typography,
+  Paper
 } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
 import { observer } from "mobx-react";
 import { observable } from "mobx";
 import UserAvatar from "../../components/UserAvatar";
@@ -41,92 +44,111 @@ class Standings extends Component {
 
   render() {
     const { tournament } = this.props;
+
     return (
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>#</TableCell>
-            <TableCell style={{ width: "100%" }} />
-            <TableCell align="right">
-              {this.renderColumnButton({ property: "games", text: "Games" })}
-            </TableCell>
-            <TableCell align="right">
-              {this.renderColumnButton({ property: "wins", text: "Wins" })}
-            </TableCell>
-            <TableCell align="right">
-              {this.renderColumnButton({
-                property: "defeats",
-                text: "Defeats"
-              })}
-            </TableCell>
-            <TableCell align="right">
-              {this.renderColumnButton({
-                property: "goalsScored",
-                text: "Goals Scored"
-              })}
-            </TableCell>
-            <TableCell align="right">
-              {this.renderColumnButton({
-                property: "goalsMissed",
-                text: "Goals Missed"
-              })}
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tournament.stats
-            .sort((a, b) => b[this.sortingProperty] - a[this.sortingProperty])
-            .map((item, index) => (
-              <TableRow key={index}>
-                <TableCell>{index + 1}</TableCell>
-                <TableCell>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "center"
-                    }}
-                  >
-                    <UserAvatar size={40} user={item.team.player1} />
-                    <UserAvatar size={40} user={item.team.player2} />
-                    <Typography style={{ marginLeft: 10 }}>
-                      {item.team.name}
-                    </Typography>
-                  </div>
-                </TableCell>
-                <TableCell align="right">
-                  {this.renderColumnValue({
-                    property: "games",
-                    text: item.games
-                  })}
-                </TableCell>
-                <TableCell align="right">
-                  {this.renderColumnValue({
-                    property: "wins",
-                    text: item.wins
-                  })}
-                </TableCell>
-                <TableCell align="right">
-                  {this.renderColumnValue({
-                    property: "defeats",
-                    text: item.defeats
-                  })}
-                </TableCell>
-                <TableCell align="right">
-                  {this.renderColumnValue({
-                    property: "goalsScored",
-                    text: item.goalsScored
-                  })}
-                </TableCell>
-                <TableCell align="right">
-                  {this.renderColumnValue({
-                    property: "goalsMissed",
-                    text: item.goalsMissed
-                  })}
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
+      <Paper>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>#</TableCell>
+              <TableCell />
+              <TableCell style={{ width: "300px" }}>TeamName</TableCell>
+              <TableCell align="center">
+                {this.renderColumnButton({ property: "games", text: "Games" })}
+              </TableCell>
+              <TableCell align="center">
+                {this.renderColumnButton({ property: "wins", text: "Wins" })}
+              </TableCell>
+              <TableCell align="center">
+                {this.renderColumnButton({
+                  property: "defeats",
+                  text: "Defeats"
+                })}
+              </TableCell>
+              <TableCell align="center">
+                {this.renderColumnButton({
+                  property: "goalsScored",
+                  text: "Goals Scored"
+                })}
+              </TableCell>
+              <TableCell align="center">
+                {this.renderColumnButton({
+                  property: "goalsMissed",
+                  text: "Goals Missed"
+                })}
+              </TableCell>
+              <TableCell />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tournament.stats
+              .sort((a, b) => b[this.sortingProperty] - a[this.sortingProperty])
+              .map((item, index) => (
+                <TableRow key={index}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center"
+                      }}
+                    >
+                      <UserAvatar size={40} user={item.team.player1} />
+                      <UserAvatar size={40} user={item.team.player2} />
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center"
+                      }}
+                    >
+                      <Typography style={{ fontWeight: 600 }}>
+                        {item.team.name}
+                      </Typography>
+                    </div>
+                  </TableCell>
+                  <TableCell align="center">
+                    {this.renderColumnValue({
+                      property: "games",
+                      text: item.games
+                    })}
+                  </TableCell>
+                  <TableCell align="center">
+                    {this.renderColumnValue({
+                      property: "wins",
+                      text: item.wins
+                    })}
+                  </TableCell>
+                  <TableCell align="center">
+                    {this.renderColumnValue({
+                      property: "defeats",
+                      text: item.defeats
+                    })}
+                  </TableCell>
+                  <TableCell align="center">
+                    {this.renderColumnValue({
+                      property: "goalsScored",
+                      text: item.goalsScored
+                    })}
+                  </TableCell>
+                  <TableCell align="center">
+                    {this.renderColumnValue({
+                      property: "goalsMissed",
+                      text: item.goalsMissed
+                    })}
+                  </TableCell>
+                  <TableCell>
+                    <IconButton aria-label="delete" color="secondary">
+                      <DeleteIcon fontSize="small" />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
