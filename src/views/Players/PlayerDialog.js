d73cdf2adb9540060ffd2c8b4f612f06d240f40f
DmitryDoskolovich <dmitriy.doskolovich@gmail.com>
2019-11-07 02:59:50 +0400

    fix naming issue and code refactoring

--- src/views/Players/PlayerDialog.js
+++ src/views/Players/PlayerDialog.js
@@ -9,243 +9,245 @@ import DialogActions from "@material-ui/core/DialogActions";
 import CancelIcon from "@material-ui/icons/Cancel";
 import ConfirmationDialog from "../../components/ConfirmationDialog";
 import Snackbar from "@material-ui/core/Snackbar";
-import IconButton from "@material-ui/core/IconButton";
-import CloseIcon from "@material-ui/icons/Close";
+import PersonIcon from "@material-ui/icons/Person";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-import styles from "./Player.style";
-import photoPlaceholder from "../../resources/Players/user-placeholder.jpg";
+import styles from "./PlayerDialog.style";
 
-const ACCEPTABLE_FILE_TYPES = ["image/png", "image/jpg", "image/jpeg"];
-const MAX_FILE_SIZE = 3145728;
+const SUPPORTED_FILE_TYPES = ["image/png", "image/jpg", "image/jpeg"];
+const MAX_FILE_SIZE = 3 * 1024 * 1024;
 
 @withStyles(styles)
 class Player extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: "",
-      name: "",
-      srcPhoto: photoPlaceholder,
-      playerIsOpen: true,
-      closePopupIsOpen: false,
-      makePlayerInactiveDialogIsOpen: false,
-      fileSizeIsError: false,
-      fileTypeIsError: false,
-      sucessSnackbarIsOpen: false,
-      playerInfoIncoming: {}
+      player: props.player,
+      message:"",
+      isFileSizeInvalid: false,
+      isFileTypeInvalid: false,
+      isDialogClosingRequested: false,
+      isUserInactivationRequested: false,
+      isMessageVisible: false
     };
   }
 
-  componentDidMount = () => {
-    const playerInfo = this.props.player;
-    this.setState({
-      email: playerInfo.email ? playerInfo.email : "",
-      name: playerInfo.name ? playerInfo.name : "",
-      srcPhoto: playerInfo.photoUrl ? playerInfo.photoUrl : photoPlaceholder,
-      playerInfoIncoming: playerInfo
-    });
-  };
+  componentDidUpdate(prevProps) {
+    if (!prevProps.open && this.props.open) {
+      this.setState({ player: this.props.player });
+    }
+  }
+
+  get isPlayerChanged() {
+    return (
+      this.props.player &&
+      this.state.player &&
+      (this.state.player.email !== this.props.player.email ||
+        this.state.player.name !== this.props.player.name ||
+        this.state.player.photoUrl !== this.props.player.photoUrl)
+    );
+  }
 
   handleChangeEmail = e => {
-    this.setState({ email: e.target.value });
+    const email = e.target.value;
+    this.setState(state => ({
+      player: { ...state.player, email }
+    }));
   };
 
   handleChangeName = e => {
-    this.setState({ name: e.target.value });
+    const name = e.target.value;
+    this.setState(state => ({
+      player: { ...state.player, name }
+    }));
   };
 
-  onClosePlayerDialog = () => {
-    if (this.checkPlayerDataIsChanged()) {
-      this.openClosePopup();
-    } else {
-      this.closePlayerDialog();
+  handleChangeAvatar = e => {
+    const [file] = e.target.files;
+
+    if (file) {
+      this.setState({
+        isFileSizeInvalid: file.size > MAX_FILE_SIZE,
+        isFileTypeInvalid: !SUPPORTED_FILE_TYPES.includes(file.type)
+      });
     }
   };
 
-  closePlayerDialog = () => {
-    this.setState({ closePopupIsOpen: false, playerIsOpen: false });
-    this.props.handleClosePlayer();
+  requestDialogClosing = () => {
+    if (this.isPlayerChanged) {
+      this.setState({ isDialogClosingRequested: true });
+    } else {
+      this.closeDialog();
+    }
   };
 
-  openClosePopup = () => {
-    this.setState({ closePopupIsOpen: true });
+  cancelDialogClosing = () => {
+    this.setState({ isDialogClosingRequested: false });
   };
 
-  closeClosePopup = () => {
-    this.setState({ closePopupIsOpen: false });
+  closeDialog = () => {
+    this.setState({ isDialogClosingRequested: false, isFileSizeInvalid: false, isFileTypeInvalid: false });
+    this.props.onClose();
   };
 
-  openPlayerInactiveDialog = e => {
-    this.setState({ makePlayerInactiveDialogIsOpen: true });
+  requestUserInactivation = e => {
+    this.setState({ isUserInactivationRequested: true });
   };
 
-  closePlayerInactiveDialog = () => {
-    this.setState({ makePlayerInactiveDialogIsOpen: false });
+  cancelUserInactivation = () => {
+    this.setState({ isUserInactivationRequested: false });
   };
 
-  makePlayerInactive = () => {
-    this.closePlayerInactiveDialog();
-    this.setState({ sucessSnackbarIsOpen: true });
-    //TODO: send request to server to make this player inactive
+  inactivateUser = () => {
+    this.setState({ isUserInactivationRequested: false });
+    // TODO: send request to server to make this player inactive
+    this.showMessage("Player was successfully deactivated");
+    this.closeDialog();
   };
 
   saveChanges = () => {
-    if (this.checkPlayerDataIsChanged()) {
-      //TODO send request to server to update the data
-      this.setState({ sucessSnackbarIsOpen: true });
+    if (this.isPlayerChanged) {
+      // TODO: send request to server to update the data
+
+      this.showMessage("Player information was successfully updated");
+      this.closeDialog();
     } else {
-      this.closePlayerDialog();
+      this.closeDialog();
     }
   };
 
-  closeSnackbar = () => {
-    this.setState({ sucessSnackbarIsOpen: false, playerIsOpen: false });
-    this.props.handleClosePlayer();
+  showMessage = message => {
+    this.setState({ message, isMessageVisible: true });
   };
 
-  fileValidation = e => {
-    const file = e.target.files;
-    if (file.length !== 0) {
-      if (
-        ACCEPTABLE_FILE_TYPES.includes(file[0].type) &&
-        file[0].size <= MAX_FILE_SIZE
-      ) {
-        this.setState({ fileSizeIsError: false, fileTypeIsError: false });
-        //TODO: send request to server to update the user's avatar
-      } else if (
-        ACCEPTABLE_FILE_TYPES.includes(file[0].type) === false &&
-        file[0].size <= MAX_FILE_SIZE
-      ) {
-        this.setState({ fileTypeIsError: true, fileSizeIsError: false });
-      } else if (
-        ACCEPTABLE_FILE_TYPES.includes(file[0].type) === true &&
-        file[0].size > MAX_FILE_SIZE
-      ) {
-        this.setState({ fileSizeIsError: true, fileTypeIsError: false });
-      } else {
-        this.setState({ fileSizeIsError: true, fileTypeIsError: true });
-      }
-    }
-  };
-
-  checkPlayerDataIsChanged = () => {
-    if (
-      this.state.playerInfoIncoming.email === this.state.email &&
-      this.state.playerInfoIncoming.name === this.state.name &&
-      this.state.playerInfoIncoming.srcPhoto === this.state.photoUrl
-    ) {
-      return false;
-    } else {
-      return true;
-    }
+  closeMessage = () => {
+    this.setState({ isMessageVisible: false });
   };
 
   render() {
-    const { classes } = this.props;
+    const { classes, open } = this.props;
+    const {
+      player,
+      message,
+      isFileSizeInvalid,
+      isFileTypeInvalid
+    } = this.state;
     return (
-      <Dialog
-        open={this.state.playerIsOpen}
-        onClose={this.onClosePlayerDialog}
-        className={classes.player}
-        fullWidth={true}
-        maxWidth="xs"
-      >
-        <DialogTitle>Edit player information</DialogTitle>
-        <DialogContent className={classes.player__content}>
-          <Avatar
-            className={classes.player__avatar}
-            src={this.state.srcPhoto}
-          />
-          <input
-            accept="image/jpg,image/png"
-            id="text-button-file"
-            type="file"
-            hidden
-            onChange={this.fileValidation}
-          />
-          <label htmlFor="text-button-file">
-            <Button component="span" className={classes.player__uploadButton}>
-              Upload photo
-            </Button>
-          </label>
-          {this.state.fileSizeIsError ? (
-            <Typography variant="subtitle2" gutterBottom color="secondary">
-              File size exceeds 3 Mb.
-            </Typography>
-          ) : null}
-          {this.state.fileTypeIsError ? (
-            <Typography variant="subtitle2" gutterBottom color="secondary">
-              You can upload only .png or .jpg file.
-            </Typography>
-          ) : null}
-          <TextField
-            className={classes.player__input}
-            label="Player name"
-            value={this.state.name}
-            margin="normal"
-            onChange={this.handleChangeName}
-          />
-          <TextField
-            className={classes.player__input}
-            color="secondary"
-            label="E-mail"
-            value={this.state.email}
-            margin="normal"
-            onChange={this.handleChangeEmail}
-            type="email"
-          />
-        </DialogContent>
-        <DialogActions className={classes.player__buttonContainer}>
-          <Button
-            variant="outlined"
-            color="secondary"
-            startIcon={<CancelIcon />}
-            onClick={this.openPlayerInactiveDialog}
-          >
-            Make player inactive
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={this.saveChanges}
-          >
-            Save
-          </Button>
-        </DialogActions>
-        <ConfirmationDialog
-          open={this.state.closePopupIsOpen}
-          handleClose={this.closeClosePopup}
-          title="Close popup"
-          contentText="Are you sure you want to close the popup? All changes will be lost."
-          handleConfirm={this.closePlayerDialog}
-        />
-        <ConfirmationDialog
-          open={this.state.makePlayerInactiveDialogIsOpen}
-          handleClose={this.closePlayerInactiveDialog}
-          title="Make player inactive"
-          contentText={`Are you sure you want to make player ${this.state.name} inactive?`}
-          handleConfirm={this.makePlayerInactive}
-        />
-        <Snackbar
-          open={this.state.sucessSnackbarIsOpen}
-          //autoHideDuration={10000}
-          onClose={this.closeSnackbar}
-          ContentProps={{
-            className: classes.player__snackbarContent
-          }}
-          message={<span>Player information was successfully updated</span>}
-          action={[
-            <IconButton
-              key="close"
-              color="inherit"
-              onClick={this.closeSnackbar}
+      <>
+        <Dialog
+          open={open}
+          onClose={this.requestDialogClosing}
+          className={classes.player}
+          fullWidth={true}
+          maxWidth="xs"
+        >
+          <DialogTitle>Edit player information</DialogTitle>
+          {player && (
+            <DialogContent className={classes.player__content}>
+              <div className={classes.player__avatarContainer}>
+                <Avatar
+                  src={player.photoUrl}
+                  className={classes.player__avatar}
+                >
+                  {!player.photoUrl && (
+                    <PersonIcon className={classes.player__avatarIcon} />
+                  )}
+                </Avatar>
+                <input
+                  accept="image/jpg,image/png"
+                  id="text-button-file"
+                  type="file"
+                  hidden
+                  onChange={this.handleChangeAvatar}
+                />
+                <label htmlFor="text-button-file">
+                  <Button
+                    component="span"
+                    className={classes.player__uploadButton}
+                  >
+                    Upload photo
+                  </Button>
+                </label>
+                {isFileSizeInvalid ? (
+                  <Typography
+                    variant="subtitle2"
+                    gutterBottom
+                    color="secondary"
+                  >
+                    File size exceeds 3 Mb.
+                  </Typography>
+                ) : null}
+                {isFileTypeInvalid ? (
+                  <Typography
+                    variant="subtitle2"
+                    gutterBottom
+                    color="secondary"
+                  >
+                    You can upload only .png or .jpg file.
+                  </Typography>
+                ) : null}
+              </div>
+              <TextField
+                fullWidth
+                label="Player name"
+                value={player.name ? player.name : ""}
+                margin="normal"
+                onChange={this.handleChangeName}
+              />
+
+              <TextField
+                fullWidth
+                color="secondary"
+                label="E-mail"
+                value={player.email ? player.email : ""}
+                margin="normal"
+                onChange={this.handleChangeEmail}
+                type="email"
+              />
+            </DialogContent>
+          )}
+          <DialogActions className={classes.player__buttonContainer}>
+            <Button
+              className={classes.player__inactivateButton}
+              variant="outlined"
+              color="secondary"
+              startIcon={<CancelIcon />}
+              onClick={this.requestUserInactivation}
             >
-              <CloseIcon />
-            </IconButton>
-          ]}
+              Make player inactive
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={this.saveChanges}
+            >
+              Save
+            </Button>
+          </DialogActions>
+          <ConfirmationDialog
+            open={this.state.isDialogClosingRequested}
+            handleClose={this.cancelDialogClosing}
+            title="Close popup"
+            contentText="Are you sure you want to close the popup? All changes will be lost."
+            handleConfirm={this.closeDialog}
+          />
+          <ConfirmationDialog
+            open={this.state.isUserInactivationRequested}
+            handleClose={this.cancelUserInactivation}
+            title="Make player inactive"
+            contentText={`Are you sure you want to make player ${
+              player ? player.name : ""
+            } inactive?`}
+            handleConfirm={this.inactivateUser}
+          />
+        </Dialog>
+        <Snackbar
+          open={this.state.isMessageVisible}
+          autoHideDuration={3000}
+          onClose={this.closeMessage}
+          message={message}
         />
-      </Dialog>
+      </>
     );
   }
 }
