dcd88726341be6e37ce9accef563e1c38d9d3322
blotteracid <klubniiikaaa@mail.ru>
2019-11-12 23:47:39 +0400

    Add infinite scroll, emulation lazyload data

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -4,14 +4,52 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
+import InfiniteScroll from "react-infinite-scroller";
 
 class DataTable extends Component {
   constructor(props) {
     super(props);
+
+    // Emulation page load
+    this.state = {
+      allData: props.data,
+      currentData: [],
+      hasMore: true
+    };
   }
 
+  loadMore = page => {
+    const { allData, currentData } = this.state;
+    const start = (page - 1) * 20;
+    const step = 20;
+    const pageSlice = allData.slice(start, start + step);
+
+    if (currentData.length >= allData.length) {
+      this.setState({ hasMore: false });
+    } else {
+      this.setState({
+        currentData: [...currentData, ...pageSlice]
+      });
+    }
+  };
+
   render() {
-    const { data, columns } = this.props;
+    const { columns } = this.props;
+    const { currentData, hasMore } = this.state;
+
+    const infiniteScrollItems = [];
+
+    currentData.map((row, i) => {
+      infiniteScrollItems.push(
+        <TableRow key={i}>
+          {columns.map((column, k) => (
+            <TableCell key={k}>
+              {column.render ? column.render(row) : row[column.field]}
+            </TableCell>
+          ))}
+        </TableRow>
+      );
+    });
 
     return (
       <Table>
@@ -22,19 +60,15 @@ class DataTable extends Component {
             ))}
           </TableRow>
         </TableHead>
-        <TableBody>
-          {data.map((row, i) => {
-            return (
-              <TableRow key={i}>
-                {columns.map((column, k) => (
-                  <TableCell key={k}>
-                    {column.render ? column.render(row) : row[column.field]}
-                  </TableCell>
-                ))}
-              </TableRow>
-            );
-          })}
-        </TableBody>
+
+        <InfiniteScroll
+          element={TableBody}
+          loadMore={this.loadMore}
+          hasMore={hasMore}
+          pageStart={0}
+        >
+          {infiniteScrollItems}
+        </InfiniteScroll>
       </Table>
     );
   }
