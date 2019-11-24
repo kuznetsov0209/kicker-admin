d0bd0c26ac1b1aabe06730fcee99f6d036eb6f85
blotteracid <klubniiikaaa@mail.ru>
2019-11-24 23:59:48 +0400

    Add dropZone on all page

--- src/views/Players/PlayerDialog.style.js
+++ src/views/Players/PlayerDialog.style.js
@@ -18,6 +18,30 @@ const styles = theme => ({
   },
   player__inactivateButton: {
     marginRight: "auto"
+  },
+  player__dropZone: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "fixed",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    zIndex: 9999,
+    pointerEvents: "none"
+  },
+  player__dropZoneActive: {
+    background: "rgba(255, 255, 255, 0.75)",
+    border: "5px dashed #fff"
+  },
+  player__dropZoneText: {
+    fontSize: "30px",
+    color: "#000",
+    display: "none"
+  },
+  player__dropZoneTextActive: {
+    display: "block"
   }
 });
 
